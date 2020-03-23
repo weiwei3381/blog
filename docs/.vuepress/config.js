f96b25b242fccf4054a13c84414fa070cdac4768
weiwei3381 <272885904@qq.com>
2020-03-23 17:17:40 +0800

    博客基本框架搭建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,30 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around',
     // 提交到gitee则开启base
-    // base:'/blogs/'
+    base:'/blogs/',
+    title: '贺小熊的技术Blog',
+    description: '衣带渐宽终不悔',
+    themeConfig: {
+        // 添加导航栏
+        nav: [
+          { text: '前端', link: '/front-end/' },
+          { text: '后端', link: '/back-end/' },
+          { text: '办公', link: '/office/' },
+          { text: '通用', link: '/general/' },
+          {
+            text: '资源',
+            // 这里是下拉列表展现形式。
+            items: [
+              { text: '小鸟搜索', link: 'https://www.birdiesearch.com/' },
+              { text: '每天一本编程书', link: 'https://salttiger.com/' },
+              { text: '掘金', link: 'https://juejin.im/' },
+            ]
+          }
+        ],
+        // 为以下路由添加侧边栏
+        sidebar: {
+            '/front-end/': [
+              'vuepress',
+            ]
+          }
+      }
   }
--- docs/README.md
+++ docs/README.md
@@ -1,9 +1,14 @@
-# 一级标题
-
-## 二级标题
-
-### 三级标题
-
-进行测试用的GitHub page。
-
-今天天气不错。
+---
+home: true
+heroImage: /hero.png
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+- title: 简洁至上
+  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: Vue驱动
+  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+- title: 高性能
+  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+footer: MIT Licensed | © Copyright 2019 贺小熊 - All Rights Reserved
+---
