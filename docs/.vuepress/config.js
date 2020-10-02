e28e86dda6e507806b5535a48a6d6facbb4a69dc
wowbat <weiwei3381@live.com>
2020-10-03 07:31:45 +0800

    add: 增加英国国防框架

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -77,7 +77,7 @@ module.exports = {
       // 通用技术
       '/general/': ['git', 'markdown', 'nexus', 'latex', 'vscode', 'guide', 'bone', 'reg'],
       // 论文
-      '/paper/': ['search', 'defense', 'cover','2020Auguest','daily',],
+      '/paper/': ['search', 'defense', 'cover','2020Auguest','daily','technology'],
     },
   },
   // vuepress插件系统
